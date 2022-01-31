ebd6320cc74a0a58ba0ecd6c23bdb49dd4355fe9
Md Shihab Uddin <uddinmdshihab452@gmail.com>
2022-01-31 21:03:14 +0600

    last commit

--- compair-3-value..js
+++ compair-3-value..js
@@ -1,3 +1,5 @@
+// ============output big number in three number============
+
 // simple use if else if else
 
 
@@ -13,15 +15,88 @@
 //     console.log('number3', num3, 'is big other two number')
 // }
 
+
+// use nasted function
+
+// var number1 = 1000;
+// var number2 = 6000;
+// var number3 = 7000;
+
+// if (number1 > number2) {
+//     if (number1 > number3) {
+//         console.log("Number1 is big");
+//     }
+// } else if (number2 > number3) {
+//     if (number2 > number1) {
+//         console.log("Number2 is big");
+//     }
+// } else {
+//     console.log("Number3 is big");
+// }
+
 // use function
 
-function putValue(num1, num2, num3) {
-    if (num1 > num2 && num1 > num3) {
-        console.log('number1', num1, 'is big other two number');
-    } else if (num2 > num1 && num2 > num3) {
-        console.log('number2', num2, 'is big other two number');
-    } else {
-        console.log('number3', num3, 'is big other two number')
-    }
-}
-putValue(300, 500, 250);
+// function putValue(num1, num2, num3) {
+//     if (num1 > num2 && num1 > num3) {
+//         console.log('number1', num1, 'is big other two number');
+//     } else if (num2 > num1 && num2 > num3) {
+//         console.log('number2', num2, 'is big other two number');
+//     } else {
+//         console.log('number3', num3, 'is big other two number')
+//     }
+// }
+// putValue(300, 500, 250);
+
+
+
+
+
+// ============output small number in three number============
+
+
+// simple use if else if else
+
+
+
+// var num1 = 3000;
+// var num2 = 300;
+// var num3 = 19900;
+// if (num1 < num2 && num1 < num3) {
+//     console.log('number1', num1, 'is small other two number');
+// } else if (num2 < num1 && num2 < num3) {
+//     console.log('number2', num2, 'is small other two number');
+// } else {
+//     console.log('number3', num3, 'is small other two number')
+// }
+
+
+// use nasted function
+
+// var number1 = 1000;
+// var number2 = 6000;
+// var number3 = 7000;
+
+// if (number1 < number2) {
+//     if (number1 < number3) {
+//         console.log("Number1 is big");
+//     }
+// } else if (number2 < number3) {
+//     if (number2 < number1) {
+//         console.log("Number2 is big");
+//     }
+// } else {
+//     console.log("Number3 is big");
+// }
+
+// use function
+
+// function putValue(num1, num2, num3) {
+//     if (num1 < num2 && num1 < num3) {
+//         console.log('number1', num1, 'is big other two number');
+//     } else if (num2 < num1 && num2 < num3) {
+//         console.log('number2', num2, 'is big other two number');
+//     } else {
+//         console.log('number3', num3, 'is big other two number')
+//     }
+// }
+// putValue(300, 500, 250);
