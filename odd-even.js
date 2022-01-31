9218b254da4b97abc3b261ae5c411e0c549cb6d0
Md Shihab Uddin <uddinmdshihab452@gmail.com>
2022-01-31 21:20:50 +0600

    odd-even

---
+++ odd-even.js
@@ -0,0 +1,43 @@
+// ===============out put odd number================================
+
+
+// use for loop
+
+// for (var i = 1; i <= 100; i++) {
+//     if (i % 2 != 0) {
+//         console.log(i);
+//     }
+// }
+
+// use while loop
+
+// var i = 1;
+// while (i <= 100) {
+//     if (i % 2 != 0) {
+//         console.log(i);
+//     };
+//     i++;
+// }
+
+
+// ===============out put even number================================
+
+
+
+// use for loop
+
+// for (var i = 1; i <= 100; i++) {
+//     if (i % 2 == 0) {
+//         console.log(i);
+//     }
+// }
+
+// use while loop
+
+// var i = 1;
+// while (i <= 100) {
+//     if (i % 2 == 0) {
+//         console.log(i);
+//     };
+//     i++;
+// }
