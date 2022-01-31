39cbe46b2ab7291f06d3fafe57e52d05ef1dda30
Md Shihab Uddin <uddinmdshihab452@gmail.com>
2022-01-31 21:33:21 +0600

    namta

---
+++ namta.js
@@ -0,0 +1,18 @@
+// using for loop
+
+// var number = 7;
+// for (i = 1; i <= 10; i++) {
+//     var result = number * i;
+//     console.log(number, 'x', i, '=', result)
+// }
+
+// using while loop
+
+
+// var number = 8;
+// var i = 1;
+// while (i <= 10) {
+//     var result = number * i;
+//     console.log(number, 'x', i, '=', result);
+//     i++;
+// }
