91ad8851cbe4ebe77fadffadd3e992e026f2f379
Md Shihab Uddin <uddinmdshihab452@gmail.com>
2022-01-31 20:41:01 +0600

    vowel-constant

---
+++ vowel-constant.js
@@ -0,0 +1,25 @@
+// simple use if else
+
+// var input = 'z';
+// var letter = input.toLowerCase();
+// if (letter == 'a' || letter == 'e' || letter == 'i' || letter == 'o' || letter == 'u') {
+//     console.log(letter, 'is vowel')
+// } else {
+//     console.log(letter, 'is conostant')
+// }
+
+
+
+// use function
+
+
+
+function getValue(input) {
+    var letter = input.toLowerCase();
+    if (letter == 'a' || letter == 'e' || letter == 'i' || letter == 'o' || letter == 'u') {
+        console.log(letter, 'is vowel');
+    } else {
+        console.log(letter, 'is conostant');
+    }
+}
+getValue('T');
