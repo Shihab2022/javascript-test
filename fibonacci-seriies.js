8fd500673c086ea1cef70759bcad142a297e3440
Md Shihab Uddin <uddinmdshihab452@gmail.com>
2022-02-01 22:07:08 +0600

    first commit

---
+++ fibonacci-seriies.js
@@ -0,0 +1,26 @@
+// fibonacci series 0 1 1 2 3 5 8 13 21 35 46 ..............
+
+/*const fibo = [0, 1];
+for (let i = 2; i <= 15; i++) {
+    fibo[i] = fibo[i - 1] + fibo[i - 2];
+}
+console.log(fibo);
+*/
+
+// use function
+
+// function fiboNacciSeries(num) {
+//     if (typeof num != 'number') {
+//         return 'Please give a number'
+//     }
+//     if (num < 2) {
+//         return 'Please give a positive number is grader than 1'
+//     }
+//     const fibo = [0, 1]
+//     for (let i = 2; i < num; i++) {
+//         fibo[i] = fibo[i - 1] + fibo[i - 2];
+//     }
+//     return fibo;
+// }
+// const fiboSerice = fiboNacciSeries(13);
+// console.log(fiboSerice)
