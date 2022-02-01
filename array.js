8ab8ba742c30993c16d9301382bb710c9cedf61b
Md Shihab Uddin <uddinmdshihab452@gmail.com>
2022-02-01 21:34:45 +0600

    use fonction

--- array.js
+++ array.js
@@ -26,16 +26,33 @@
 
 // find the largest element of an array
 
-function largestElement(numbers) {
-    let largest = numbers[0];
-    for (let i = 0; i < numbers.length; i++) {
-        const element = numbers[i];
-        if (element > largest) {
-            largest = element;
-        }
-    }
-    return largest;
-}
-const ages = [10, 25, 91, 77, 10, 50, 60, 25, 14, 80, 75, 94];
-const oldest = largestElement(ages);
-console.log(oldest);
+// function largestElement(numbers) {
+//     let largest = numbers[0];
+//     for (let i = 0; i < numbers.length; i++) {
+//         const element = numbers[i];
+//         if (element > largest) {
+//             largest = element;
+//         }
+//     }
+//     return largest;
+// }
+// const ages = [10, 25, 91, 77, 10, 50, 60, 25, 14, 80, 75, 94];
+// const oldest = largestElement(ages);
+// console.log(oldest);
+
+
+// find the lowest element of an array
+
+// function lowestElement(numbers) {
+//     let lowest = numbers[0];
+//     for (let i = 0; i < numbers.length; i++) {
+//         const element = numbers[i];
+//         if (element < lowest) {
+//             lowest = element;
+//         }
+//     }
+//     return lowest;
+// }
+// const ages = [10, 25, 91, 77, 10, 50, 60, 25, 14, 80, 75, 94];
+// const oldest = lowestElement(ages);
+// console.log(oldest);
