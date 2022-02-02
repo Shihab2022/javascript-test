c4a1651c0c6a684b571994f58264b2414fa9bb6d
Md Shihab Uddin <uddinmdshihab452@gmail.com>
2022-02-02 22:27:09 +0600

    first commit

---
+++ object.js
@@ -0,0 +1,29 @@
+// akti object ar modday thakay jodi kono akta object ar tulona kortay chai taholay 
+/*phones = [
+    { name: 'samsung m31', price: 25500, camera: '64px', storage: '128gb', color: 'black' },
+    { name: 'mi', price: 11500, camera: '10px', storage: '64gb', color: 'white' },
+    { name: '1+', price: 11000, camera: '32px', storage: '128gb', color: 'black' },
+    { name: 'oppo', price: 15200, camera: '50px', storage: '64gb', color: 'gray' },
+    { name: 'nokia', price: 5000, camera: '40px', storage: '10gb', color: 'black' },
+    { name: 'motorala', price: 8000, camera: '40px', storage: '128gb', color: 'neviblue' },
+    { name: 'symphoni', price: 1500, camera: '48px', storage: '32gb', color: 'black' },
+    { name: 'walton', price: 9000, camera: '61px', storage: '64gb', color: 'black' },
+]*/
+
+/*let cheapest = phones[0];
+for (const phone of phones) {
+    if (phone.price < cheapest.price) {
+        cheapest = phone;
+    }
+}
+console.log(cheapest); */
+
+
+// for biggest prise
+/*let biggest = phones[0];
+for (const phone of phones) {
+    if (phone.price > biggest.price) {
+        biggest = phone;
+    }
+}
+console.log(biggest); */
