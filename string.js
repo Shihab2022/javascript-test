190c177c4a944214d6874cc5b44c6f1b4300760f
Md Shihab Uddin <uddinmdshihab452@gmail.com>
2022-02-02 21:22:39 +0600

    first commit

---
+++ string.js
@@ -0,0 +1,26 @@
+// string length
+
+// let myString = 'whats you  name';
+// console.log(myString.length);
+
+// string value position
+
+/*let myString = 'whats you  name';
+console.log(myString[4])
+console.log(myString[6])
+console.log(myString[14])*/
+
+
+// string reverse function
+
+
+/*let myString = 'whats you  name';
+function reverseString(text) {
+    let reverse = '';
+    for (const letter of text) {
+        reverse = letter + reverse;
+    }
+    return reverse;
+}
+const reversed = reverseString(myString);
+console.log(reversed);*/
