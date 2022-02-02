bc22c9a57d25f31b665cab964f5e85a5623bc871
Md Shihab Uddin <uddinmdshihab452@gmail.com>
2022-02-02 22:02:42 +0600

    first commit '+

---
+++ change-value-in-loop.js
@@ -0,0 +1,17 @@
+// akta loop ar modday bivinno position ay bivinno valo changre korar jjonno 
+//akta loopa ar moday onak gulo condition thakay taholay jay tai conditio basi thakbay sayta agay dibo  jamon ay  (i % 3 == 0 && i % 5 == 0)  tai condition basu asay tai agay diay si
+
+
+
+/*for (i = 0; i <= 50; i++) {
+    if (i % 3 == 0 && i % 5 == 0) {
+        console.log('this value is divided by 3 and 5')
+    } else if (i % 3 == 0) {
+        console.log('this value is divided by 3 ')
+    } else if (i % 5 == 0) {
+        console.log('this value is divided by  5')
+    } else {
+        console.log(i);
+    }
+}
+*/
