7b4505ee2a56641cc00872a352f6e70ed83a16c2
Md Shihab Uddin <uddinmdshihab452@gmail.com>
2022-02-02 21:52:44 +0600

    first commti

---
+++ multiplication-wood-requarment.js
@@ -0,0 +1,18 @@
+// ami jodi poriman chear table bed croy kortay chai say sonka ta parameter ay diat diboo and pottak ar dam perChearwood ay diay dibo taholay amder sob calculation diay dibay
+
+
+/*function woodCalculate(chearQuantity, tableQuantity, bedQuantity) {
+    const perChearWood = 3;
+    const perTableWood = 15;
+    const perBedWood = 55;
+
+    const chearWoodQuantity = perChearWood * chearQuantity;
+    const tableWoodQuantity = perTableWood * tableQuantity;
+    const bedWoodQuantity = perBedWood * bedQuantity;
+
+    const totalWoodQuantity = chearWoodQuantity + tableWoodQuantity + bedWoodQuantity;
+    return totalWoodQuantity;
+}
+
+const firstOption = woodCalculate(0, 0, 1);
+console.log(firstOption);*/
