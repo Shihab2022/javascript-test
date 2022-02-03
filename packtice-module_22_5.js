46f09adcf796a9a39c478516aa052251a11d8770
Md Shihab Uddin <uddinmdshihab452@gmail.com>
2022-02-03 08:16:14 +0600

    first commit

---
+++ packtice-module_22_5.js
@@ -0,0 +1,82 @@
+/*************************************** * (7) 7 to 19 odd number prind********************************/
+
+
+/*let num1 = 7;
+while (num1 <= 19) {
+    if (num1 % 2 != 0) {
+        console.log(num1);
+    }
+    num1++;
+}*/
+
+
+/*************************************** *  (8) declear array and its position********************************/
+
+
+// let myArray = [2, 6, 6, 8, 8, 3, 86, 53, 34, 7, 5, 433, 567, 7, 55562, 3456, 332]
+
+// length
+/*let lengthArray = myArray.length;
+console.log(lengthArray);*/
+
+// change 4th position element
+
+/*myArray[4] = 30000;
+console.log(myArray);*/
+
+// array push
+
+/*myArray.push(5000, 50);
+console.log(myArray);*/
+
+
+// array pop
+
+/*myArray.pop();
+console.log(myArray);*/
+
+/***********************  (9) for  loop array ,for of loop array and its position******************/
+
+/*let names = ['shihab', 'shiraj', 'faruk', 'thorul', 'khafi', 'mamun'];
+
+for (const name of names) {
+    console.log(name);
+}*/
+
+
+/***********************  (10) print the value gater than 80 and its position******************/
+
+/*let elements = [2, 6, 450, 666, 6, 80, 80, 3, 86, 53, 34, 7, 5, 433, 567, 7, 55562, 3456, 332]
+
+for (const element of elements) {
+    if (element >= 80) {
+        console.log(element);
+    }
+}*/
+
+
+/***********************  (11) multifikaytion 3 valuse ******************/
+
+/*function multifikaytion(num1, num2, num3) {
+    let multi = num1 * num2 * num3;
+    return multi;
+}
+let value = multifikaytion(10, 20, 30);
+console.log(value);
+*/
+
+
+/***********************  (12) change object property******************/
+
+education = [
+    { examName: 'PSE', bord: 'rajshahi', year: 2011, gpa: 4.41 },
+    { examName: 'JDC', bord: 'madrasah', year: 2015, gpa: 4.82 },
+    { examName: 'DHAKIL', bord: 'madrasah', year: 2017, gpa: 4.61 },
+    { examName: 'HSC', bord: 'rajshahi', year: 2019, gpa: 4.42 },
+
+]
+
+
+
+
+/***********************  (10) print the value gater than 80 and its position******************/
